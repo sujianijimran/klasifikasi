f3e08d9ebaa32d44cf4e61946227dba514b45d9e
Are1PM <arwanpriantomangidi@gmail.com>
2021-12-13 17:34:42 +0800

    [UPDATE] Ubah tampilan klasifikasi

--- app/Models/Komentar.php
+++ app/Models/Komentar.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 class Komentar extends Model
 {
     protected $table = "mengomentari";
-    // protected $primaryKey = "mengomentari";
+    protected $primaryKey = "id_komentar";
     protected $allowedFields = ["pelanggan_id", "produk_id", "komentar", "kesimpulan_id"];
 }
 ?>
